f527576e27c777492297d8a3ccd1cfa0bcf32e35
Ben Olive <sionide21@gmail.com>
2016-10-11 19:37:24 -0400

    Configure iex to use the serial port

--- apps/fw/config/config.exs
+++ apps/fw/config/config.exs
@@ -5,6 +5,8 @@
 # is restricted to this project.
 use Mix.Config
 
+config :nerves, :firmware, rootfs_additions: "rootfs-additions"
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
